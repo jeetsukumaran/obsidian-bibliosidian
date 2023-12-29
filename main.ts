b32e7b5f82e1d9f937c1c5ddc00694535c61bc5b
Jeet Sukumaran <jeetsukumaran@gmail.com>
2023-12-29 15:36:32 -0800

    .

--- main.ts
+++ main.ts
@@ -128,18 +128,8 @@ export default class Bibliosidian extends Plugin {
 			bibtexModal.open();
 		});
 
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new BibliosidianSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		// this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-		// 	console.log('click', evt);
-		// });
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		// this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -186,13 +176,18 @@ class BibliosidianSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName("References: source properties prefix")
+			.setDesc(`
+This will be prefixed to the normalized bibliographic data fields for reference bibliographic data.
+For example, if set to 'source-', the frontmatter YAML field will be 'source-authors' instead of just 'authors'.
+Better namespacing will come when Obsidian supports nested frontmatter YAML objects.
+`
+			)
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder("(e.g., 'source-')")
+				.setValue(this.plugin.settings.referenceSourcePropertiesPrefix)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.referenceSourcePropertiesPrefix = value;
 					await this.plugin.saveSettings();
 				}));
 	}
