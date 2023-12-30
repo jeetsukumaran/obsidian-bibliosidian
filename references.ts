3a7beb1f7d96fc6cdc1befc1799df4d6e58600f7
Jeet Sukumaran <jeetsukumaran@gmail.com>
2023-12-29 18:30:04 -0800

    .

--- references.ts
+++ references.ts
@@ -19,6 +19,13 @@ import {
 	BibEntry,
 	FieldValue,
 } from "bibtex";
+
+
+// npm install yaml
+// import YAML from "yaml";
+const YAML = require('yaml')
+
+
 // import { parseBibFile } from "bibtex";
 import * as _path from "path";
 
@@ -91,7 +98,7 @@ export function generateSourceFrontmatter(
 		"sources/authors", // abstract away later; path to that author notes are stored
 	)
 	if (authorLinks) {
-		updateYAMLProperty(
+		replaceYAMLProperty(
 			app,
 			targetFilePath,
 			authorsYamlLabel,
@@ -163,6 +170,74 @@ export async function updateYAMLProperty(
         const frontMatterMatch = content.match(frontmatterRegex);
         if (frontMatterMatch) {
             let frontmatter = frontMatterMatch[1];
+            let frontmatterLines: string[] = frontmatter.split("\n");
+            let updatedLines: string[] = []
+            let isUpdatedExisting = false
+            frontmatterLines.forEach( (line: string) => {
+				// let propRx = new RegExp(`^(\\s+)${propertyName}`);
+				let propRx = new RegExp(`^(\\s+)${propertyName}`);
+				// let propRx = new RegExp(`^(\\s+)${propertyName}\\s*:\\s*$`);
+				let propMatch = line.match(/source-authors:/)
+				if (propMatch) {
+					// rewrite field name in case it had inline data/values
+					// TODO: save these?
+					updatedLines.push(`${propertyName}:`)
+					// insert new rows here;
+					// previous entries will be appended after as loop proceeds
+					updatedLines.push(... updatedRows)
+					isUpdatedExisting = true
+				} else {
+					updatedLines.push(line)
+				}
+            })
+            if (!isUpdatedExisting) {
+            	updatedLines.push(`${propertyName}:`)
+            	updatedLines.push(... updatedRows)
+            }
+            newYAML = updatedLines.join("\n");
+            content = content.replace(frontmatterRegex, `---\n${newYAML}\n---`);
+        } else {
+            newYAML = `---\n${propertyName}:\n${updatedRows.join("\n")}\n---`;
+            content = newYAML + "\n" + content;
+        }
+
+        await app.vault.modify(file, content);
+    } else {
+        console.error("File not found");
+    }
+}
+
+export async function replaceYAMLProperty(
+	app: App,
+	filePath: string,
+	propertyName: string,
+	newValues: string[],
+) {
+    const file = app.vault.getAbstractFileByPath(filePath);
+    if (file instanceof TFile) {
+
+		// let frontmatter = app.metadataCache?.getFileCache(file)?.frontmatter
+		// console.log(app.metadataCache)
+		// if (frontmatter) {
+		// 	frontmatter["source-authors"] = "hello"
+		// }
+		// let frontMatter = this.metadataCache?.frontmatter
+		// console.log(frontmatter)
+
+        let content = await app.vault.read(file);
+		let updatedRows = newValues.map(value => `  - ${value}`)
+        let newYAML;
+        const frontmatterRegex = /^---\n([\s\S]*?)\n---/;
+        const frontMatterMatch = content.match(frontmatterRegex);
+        if (frontMatterMatch) {
+            let frontmatter = frontMatterMatch[1];
+			let parsedFrontmatter;
+			try {
+				parsedFrontmatter = YAML.parse(frontmatter)
+			} catch (err) {
+				console.log(err)
+			}
+            console.log(parsedFrontmatter)
             let frontmatterLines: string[] = frontmatter.split("\n");
             let updatedLines: string[] = []
             let isUpdatedExisting = false
