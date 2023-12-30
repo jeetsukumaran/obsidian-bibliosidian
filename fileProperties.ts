b7614d1183ea0350de379ab3abf7920eaba95556
Jeet Sukumaran <jeetsukumaran@gmail.com>
2023-12-30 00:32:44 -0800

    .

--- fileProperties.ts
+++ fileProperties.ts
@@ -61,68 +61,7 @@ ${err}`
 			}
 		});
 		// parsedFrontmatter[propertyName] = newValues
-        let newFrontmatterStr: string = `---\n${YAML.stringify(parsedFrontmatter)}\n---`
-
-        if (frontMatterMatch) {
-			content = content.replace(frontmatterRegex, newFrontmatterStr);
-		} else {
-			content = newFrontmatterStr + "\n" + content
-		}
-        await app.vault.modify(file, content);
-    } else {
-        console.error("File not found");
-    }
-
-	// let propertyName = bibToYamlLabelFn(bibTexField)
-	// if (values) {
-	// 	updateFrontmatterYaml(
-	// 		app,
-	// 		targetFilePath,
-	// 		propertyName,
-	// 		values,
-	// 	)
-	// } else {
-	// 	// clear?
-	// }
-}
-
-export async function updateFrontmatterYaml(
-	app: App,
-	filePath: string,
-	propertyName: string,
-	newValues: string | string[],
-) {
-    const file = app.vault.getAbstractFileByPath(filePath);
-    if (file instanceof TFile) {
-
-		// let frontmatter = app.metadataCache?.getFileCache(file)?.frontmatter
-		// if (frontmatter) {
-		// 	frontmatter["source-authors"] = "hello"
-		// }
-		// let frontMatter = this.metadataCache?.frontmatter
-
-
-        let content = await app.vault.read(file);
-
-		let parsedFrontmatter: FilePropertyData = {};
-        const frontmatterRegex = /^---\n([\s\S]*?)\n---/;
-        const frontMatterMatch = content.match(frontmatterRegex);
-        if (frontMatterMatch) {
-            let frontmatter = frontMatterMatch[1];
-			try {
-				parsedFrontmatter = { ... YAML.parse(frontmatter) }
-			} catch (err) {
-				new Notice(`
-Failed to parse YAML frontmatter from file '${filePath}':
-${err}
-			`)
-				console.log(err)
-				return
-			}
-        }
-
-		parsedFrontmatter[propertyName] = newValues
-        let newFrontmatterStr: string = `---\n${YAML.stringify(parsedFrontmatter).strip()}\n---`
+        let newFrontmatterStr: string = `---\n${YAML.stringify(parsedFrontmatter).trim()}\n---`
 
         if (frontMatterMatch) {
 			content = content.replace(frontmatterRegex, newFrontmatterStr);
@@ -134,4 +73,53 @@ ${err}
         console.error("File not found");
     }
 }
+
+// export async function updateFrontmatterYaml(
+// 	app: App,
+// 	filePath: string,
+// 	propertyName: string,
+// 	newValues: string | string[],
+// ) {
+//     const file = app.vault.getAbstractFileByPath(filePath);
+//     if (file instanceof TFile) {
+
+// 		// let frontmatter = app.metadataCache?.getFileCache(file)?.frontmatter
+// 		// if (frontmatter) {
+// 		// 	frontmatter["source-authors"] = "hello"
+// 		// }
+// 		// let frontMatter = this.metadataCache?.frontmatter
+
+
+//         let content = await app.vault.read(file);
+
+// 		let parsedFrontmatter: FilePropertyData = {};
+//         const frontmatterRegex = /^---\n([\s\S]*?)\n---/;
+//         const frontMatterMatch = content.match(frontmatterRegex);
+//         if (frontMatterMatch) {
+//             let frontmatter = frontMatterMatch[1];
+// 			try {
+// 				parsedFrontmatter = { ... YAML.parse(frontmatter) }
+// 			} catch (err) {
+// 				new Notice(`
+// Failed to parse YAML frontmatter from file '${filePath}':
+// ${err}
+// 			`)
+// 				console.log(err)
+// 				return
+// 			}
+//         }
+
+// 		parsedFrontmatter[propertyName] = newValues
+//         let newFrontmatterStr: string = `---\n${YAML.stringify(parsedFrontmatter).strip()}\n---`
+
+//         if (frontMatterMatch) {
+// 			content = content.replace(frontmatterRegex, newFrontmatterStr);
+// 		} else {
+// 			content = newFrontmatterStr + "\n" + content
+// 		}
+//         await app.vault.modify(file, content);
+//     } else {
+//         console.error("File not found");
+//     }
+// }
 
