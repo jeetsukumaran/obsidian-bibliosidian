a967537755ca38322f9b06b01003a4e08276c337
Jeet Sukumaran <jeetsukumaran@gmail.com>
2023-12-31 00:31:47 -0800

    Update

--- references.ts
+++ references.ts
@@ -162,7 +162,6 @@ function generateSourceFrontmatter(
 		.trim()
 		.replace(/\n/g, " ")
 		.replace(/\s+/g, " ")
-	console.log(abstract)
 	let entryTitle = `(@${citekey}):${compositeTitle}`
 	refProperties["title"] = entryTitle
 	if (abstract) {
