cc285ede80083c7498f7015fdcb4ec9c83faeaa8
Jeet Sukumaran <jeetsukumaran@gmail.com>
2025-01-31 03:52:26 -0800

    Update

--- src/main.ts
+++ src/main.ts
@@ -276,10 +276,10 @@ export default class Bibliosidian extends Plugin {
 		// this.addRibbonIcon("book-up-2", "Create or update a single bibliographic note from BibTeX data", () => {
 		// 	this.updateBiblioNoteFromBibTex()
 		// });
-		this.addRibbonIcon("scroll-text", "Import or update reference; notes from BibTeX data", () => {
+		this.addRibbonIcon("scroll-text", "Import or update reference notes from BibTeX data", () => {
 			this.updateBiblioNoteLibraryFromBibTex()
 		});
-		this.addRibbonIcon("file-plus-2", "Attach a holding to the current note", () => {
+		this.addRibbonIcon("file-plus-2", "Import an external file as a bibliographic attachment of the current note", () => {
 			this.addHolding()
 		});
 
